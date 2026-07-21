70da41b6329994487425e4c944a8796f2ca52364
nateperk05@gmail.com <perkinsn@wit.edu>
2026-07-21 18:03:00 -0400

    Switched to using bbox to save graph of Boston

--- save_graph.py
+++ save_graph.py
@@ -5,8 +5,11 @@ import osmnx as ox
 Can change the parameters of ox.graph to specify a boundary box instead
 """
 
-save_path = "data/weymouth.graphml"
-location = "Weymouth, Massachusetts, USA"
+save_path = "data/bostonbbox.graphml" # Save path must end in .graphml extension
+location = "Weymouth, Massachusetts, USA" # Location to save (Can use different functions to create different graph)
 
-G = ox.graph.graph_from_place(location , network_type="drive", simplify=False)
+# G = ox.graph.graph_from_place(location, network_type="drive", simplify=False) # Graph from place 
+G = ox.graph.graph_from_bbox((-71.21046,42.24448,-70.92035,42.37891), network_type="drive", 
+                             simplify=False, retain_all=False, truncate_by_edge=True) # Boston BBox
+
 ox.io.save_graphml(G, filepath=save_path, gephi=False, encoding='utf-8')
