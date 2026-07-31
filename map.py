61e3bc4a86473469533ff5d50e8ac1b8b0fd8db6
David Costa <costad3@wit.edu>
2026-07-31 12:57:19 -0400

    feat: method for plotting route(s), and map.
    Also return both projected and unprojected map when loading map from file. Need unprojected if we want to manually pick start/end lat/lon points from OSM

--- map.py
+++ map.py
@@ -1,25 +1,28 @@
-import networkx as nx
-from networkx.classes.multidigraph import MultiDiGraph
 import osmnx as ox
-import agents as a
-import time
+from networkx.classes.multidigraph import MultiDiGraph
 
 class Map:
 
-    def load_map(path_to_map: str) -> MultiDiGraph:
+    def load_map(path_to_map: str) -> tuple[MultiDiGraph, MultiDiGraph]:
         G = ox.io.load_graphml(filepath=path_to_map)
         G = ox.routing.add_edge_speeds(G)
         G = ox.routing.add_edge_travel_times(G)
         Gp = ox.projection.project_graph(G)
 
-        return Gp
+        return G, Gp
 
     # TODO: Constrain return value?
     def plot_map(graph: MultiDiGraph):
-        fig, ax = ox.plot.plot_graph(projected_graph)
+        fig, ax = ox.plot.plot_graph(graph)
 
         return fig, ax
 
+    def plot_route(graph: MultiDiGraph, route):
+        fig, ax = ox.plot.plot_graph_route(graph, route=route, route_color="y", route_linewidth=6, node_size=0)
 
-projected_graph = Map.load_map("data/weymouth.graphml")
-fig, ax = Map.plot_map(projected_graph)
+        return fig, ax
+
+    def plot_routes(graph: MultiDiGraph, routes, route_colors):
+        fig, ax = ox.plot.plot_graph_routes(graph, routes=routes, route_colors=route_colors, route_linewidths=6, node_size=0)
+
+        return fig, ax
